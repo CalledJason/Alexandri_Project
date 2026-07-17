5b5946050637a7a924d3bff2d0495fe1f1d48c94
CalledJason <rendyhikmawan040@gmail.com>
2026-07-18 03:06:03 +0700

    chore: scaffold models, migrations, seeders, and factories

---
+++ backend/database/factories/JoinRequestFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\JoinRequest;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<JoinRequest>
+ */
+class JoinRequestFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ backend/database/factories/StudyGroupFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\StudyGroup;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<StudyGroup>
+ */
+class StudyGroupFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
